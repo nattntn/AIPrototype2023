2269534d9a7e493a4a47819f982d096b681fbf09
nattntn <natthanich.h@kkumail.com>
2023-12-12 10:37:40 +0700

    test firstpy

--- firstpy.py
+++ firstpy.py
@@ -1,2 +1,11 @@
+def printHello():
+    print("Hello world!")
+
+def multiplyby9(inputV):
+    print(9*inputV)
+
 if __name__ == "__main__":
     print('we are in the main function')
+    multiplyby9(20)
+    printHello()
+    
