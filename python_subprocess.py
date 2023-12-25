09cf287f7cce505e1b3adf8e669c2d0f242fd3a8
nattntn <natthanich.h@kkumail.com>
2023-12-25 16:08:36 +0700

    add input firstpy.py

--- python_subprocess.py
+++ python_subprocess.py
@@ -2,13 +2,13 @@ import subprocess #สำหรับ run terminal command
 
 if __name__ == "__main__":
     #basic terminal command
-    subprocess.run([print('first run num = 100 xx= 90')])
+    print('first run num = 100 xx= 90')
     subprocess.run(["python","firstpy.py", "--num", "100","--xx", "90"])
-    subprocess.run([print("-------------------------------------")])
-    subprocess.run([print('first run num = -10 xx= -90')])
+    print("-"*80)
+    print('second run num = -10 xx= -90')
     subprocess.run(["python","firstpy.py", "--num", "-10","--xx", "-90"])
-    subprocess.run([print("-------------------------------------")])
-    subprocess.run([print('first run num = 0')])
+    print("-"*80)
+    print('third run num = 0')
     subprocess.run(["python","firstpy.py", "--num", "0"])
-    subprocess.run([print("-------------------------------------")])
+    print("-"*80)
     
