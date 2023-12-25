ce4fd7a384999ee8085356e8c5be0e0ec23f7692
nattntn <natthanich.h@kkumail.com>
2023-12-25 15:38:20 +0700

    add input firstpy.py

--- python_subprocess.py
+++ python_subprocess.py
@@ -2,9 +2,7 @@ import subprocess #สำหรับ run terminal command
 
 if __name__ == "__main__":
     #basic terminal command
-    subprocess.run(["ls","-ltr"]) # เขียนเป็น list
-    subprocess.run(["rm","-r","/home/nattntn/testfolder1"])
-    subprocess.run(["python","firstpy.py", "--num", "66","--xx", "90"])
-    subprocess.run(["python","firstpy.py", "--num", "100","--xx", "57"])
-    subprocess.run(["python","firstpy.py", "--num", "66"])
+    subprocess.run(["python","firstpy.py", "--num", "100","--xx", "90"])
+    subprocess.run(["python","firstpy.py", "--num", "-10","--xx", "-90"])
+    subprocess.run(["python","firstpy.py", "--num", "0"])
     
