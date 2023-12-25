012b49845ed9907b4ebafcdcc7d3eb2854afff8f
nattntn <natthanich.h@kkumail.com>
2023-12-25 15:11:45 +0700

    add delete testfolder1

--- python_subprocess.py
+++ python_subprocess.py
@@ -3,3 +3,5 @@ import subprocess #สำหรับ run terminal command
 if __name__ == "__main__":
     #basic terminal command
     subprocess.run(["ls","-ltr"]) # เขียนเป็น list
+    subprocess.run(["rm","-r","~/testfolder1"])
+    subprocess.run(["cd"])
