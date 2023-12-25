33e2335276ff60d70c45a4c865e74313e59dfa44
nattntn <natthanich.h@kkumail.com>
2023-12-25 16:00:09 +0700

    add input firstpy.py

--- python_subprocess.py
+++ python_subprocess.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     #basic terminal command
     subprocess.run([print('first run num = 100 xx= 90')])
     subprocess.run(["python","firstpy.py", "--num", "100","--xx", "90"])
-    subprocess.run([print("--"*80)])
+    subprocess.run([print("-------------------------------------")])
     subprocess.run([print('first run num = -10 xx= -90')])
     subprocess.run(["python","firstpy.py", "--num", "-10","--xx", "-90"])
-    subprocess.run([print("--"*80)])
+    subprocess.run([print("-------------------------------------")])
     subprocess.run([print('first run num = 0')])
     subprocess.run(["python","firstpy.py", "--num", "0"])
-    subprocess.run([print("--"*80)])
+    subprocess.run([print("-------------------------------------")])
     
