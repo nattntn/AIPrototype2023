2e52a0d1595480e4e7259c9d38e347856579fd64
nattntn <natthanich.h@kkumail.com>
2023-12-25 15:33:29 +0700

    add input firstpy.py

--- python_subprocess.py
+++ python_subprocess.py
@@ -4,4 +4,7 @@ if __name__ == "__main__":
     #basic terminal command
     subprocess.run(["ls","-ltr"]) # เขียนเป็น list
     subprocess.run(["rm","-r","/home/nattntn/testfolder1"])
+    subprocess.run(["python","firstpy.py", "--num", "66","--xx", "90"])
+    subprocess.run(["python","firstpy.py", "--num", "100","--xx", "57"])
+    subprocess.run(["python","firstpy.py", "--num", "66"])
     
