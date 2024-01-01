04dd67a8dac88ec3325b0d0524a708069a93259c
nattntn <natthanich.h@kkumail.com>
2024-01-01 13:07:11 +0700

    add Popen

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,7 +1,9 @@
-import subprocess #สำหรับ run terminal command
+import subprocess #สำหรับ run terminal command # นำที่มี Output/~output มาแสดง
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #basic terminal command
+    #subprocess.run(["ls", "-ltr"]) # เขียนเป็น list ช่องว่าง ใช้  ,
+    #subprocess.run(["rm","-r","~/testfolder1"])#ลบ testfolder1
     print('first run num = 100 xx= 90')
     subprocess.run(["python","firstpy.py", "--num", "100","--xx", "90"])
     print("-"*80)
@@ -17,9 +19,10 @@ if __name__ == "__main__":
                                       stdout=subprocess.PIPE,
                                       stderr=subprocess.PIPE)
     out, err = process_output.communicate()
-    print(out.decode('utf-8'))
-    print(len(out.decode('utf-8')))
+    print(out.decode('utf-8')) # เรียกดู output
+    print(len(out.decode('utf-8'))) # นับจำนวนพยางค์ชนะ Output
 
 
     ##HW เขียน subprocess sum output ทั้งหมดของ command 3 อันข้างบน (ตัวเลขก่อน Hello world!)
     
+
